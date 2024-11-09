34060f581c5a0b2c9046f10e82f2c08e1157430d
Prahit Yaugand <prahityaugand@wirelessprv-10-192-72-155.near.illinois.edu>
2024-11-09 17:02:48 -0600

    Formatting

--- vocab_enhancer/src/components/flashcards.js
+++ vocab_enhancer/src/components/flashcards.js
@@ -2,19 +2,38 @@ import Flashcard from './flashcard';
 import './styles.css';
 import {questions} from './data';
 import { useState } from 'react';
+import RightArrow from '../images/right-arrow.svg';
+import LeftArrow from '../images/left-arrow.svg'
 
 function Flashcards() {
-    const [termDef, setTermDef] = useState(questions[0])
-    function handleClick() {
-        setTermDef(questions[Math.floor(Math.random() * questions.length)])
+    const [questionsNum, setQuestionNum] = useState(0)
+    function shuffleArray() {
+        for (let i = Math.min(questions.length - 1,50); i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1)); 
+            [questions[i], questions[j]] = [questions[j], questions[i]]; 
+          }
+    }
+
+    function handleRight() {
+        setQuestionNum((questionsNum + 1) % questions.length)
+    }
+
+    function handleLeft() {
+        setQuestionNum(( questionsNum - 1 + questions.length) % questions.length)
     }
 
     return (
-    <div>
-        
-        <Flashcard term={termDef} />
-        <button className='nextButton' onClick={handleClick}>
-            Next
+     <div className="flex-container">
+        <button onClick={handleLeft}>
+            <div>
+        <img className="arrows" src={LeftArrow} alt="Left Arrow"/>
+        </div>
+        </button>
+        <Flashcard term={questions[questionsNum]} />
+        <button onClick={handleRight}>
+            <div>
+        <img className="arrows" src={RightArrow} alt="Right Arrow"/>
+        </div>
         </button>
     </div>)
 }
